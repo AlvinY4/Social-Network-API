5f3594f6b8f9a26890ab8bf12f3b5d11f042bd62
Alvin Yearby <leroy.yearby@gmail.com>
2022-02-20 13:35:12 -0500

    added api routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -32,7 +32,7 @@ const userController = {
     },
 
     // create user 
-    CreateUser({ body }, res) {
+    createUser({ body }, res) {
         User.create(body)
         .then(dbUserData => res.json(dbUserData))
         .catch(err => res.status(400).json(err)); 
---
+++ routes/api/user-routes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/user-controller'); 
+
+router
+    .route('/')
+    .get()
+    .post();
+
+
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
+
+
+router
+    .route('/:id/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
+
+
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+module.exports = router;
